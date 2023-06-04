3c7edcd888ef59c03c49deba2a022056ec66e7cd
TheAlexDev23 <thealexdev23@gmail.com>
2023-06-04 13:01:14 +0200

    Add element shell rendering

--- src/elconf.c
+++ src/elconf.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <cwalk.h>
+#include <math.h>
 
 #include <pel.h>
 #include <pttui/easy.h>
@@ -32,9 +33,31 @@ int parse_input(int argc, char* argv[])
     return 0;
 }
 
+double to_radians(double deg)
+{
+    return deg * M_PI / 180.0;
+}
+
+#define SHELL_SPACE 8
 int render_element(periodic_element_t* element)
 {
+    for (int i = 0; i < element->conf.shells_amnt; i++)
+    {
+        // Draw shell
+        pel_draw_circle(filename, PEL_COLOR_BLACK, 0, 0, SHELL_SPACE * (i + 1));
 
+        int electrons = element->conf.shells[i];
+
+        float angleDiff = 360.0 / (float)electrons;
+        float angleDiffr = to_radians(angleDiff);
+        for (int j = 0; j < electrons; j++)
+        {
+            int x = sin(j * angleDiffr) * SHELL_SPACE * (i + 1);
+            int y = cos(j * angleDiffr) * SHELL_SPACE * (i + 1);
+
+            pel_draw_circle_full(filename, PEL_COLOR_RED, x, y, 2);
+        }
+    }
 }
 
 int main(int argc, char* argv[])
